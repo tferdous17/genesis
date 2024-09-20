12f13db9d308f1a4cd0377c4ffd86d9d33f6c472
tferdous17 <tferdous611@gmail.com>
2024-09-20 18:38:14 -0400

    create memory store (keydir) and basic methods

--- internal/memorystore.go
+++ internal/memorystore.go
@@ -1,1 +1,19 @@
 package internal
+
+type MemoryStore struct {
+	data map[string]string
+}
+
+func MakeMemoryStore() *MemoryStore {
+	return &MemoryStore{
+		data: make(map[string]string),
+	}
+}
+
+func (m *MemoryStore) Get(key string) string {
+	return m.data[key]
+}
+
+func (m *MemoryStore) Put(key string, value string) {
+	m.data[key] = value
+}
