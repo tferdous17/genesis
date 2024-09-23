d51f0fa3f4c0ad3b0d757020be2c6c7ceefbb486
tferdous17 <tferdous611@gmail.com>
2024-09-23 09:12:12 -0400

    create custom errors

---
+++ utils/errors.go
@@ -0,0 +1,19 @@
+package utils
+
+import "errors"
+
+var (
+	ErrEmptyKey     = errors.New("invalid key: key can not be empty")
+	ErrDuplicateKey = errors.New("invalid key: already in store")
+	ErrKeyNotFound  = errors.New("invalid key: not found or deleted")
+
+	ErrEmptyValue = errors.New("invalid value: value can not be empty")
+
+	ErrKeyDirInit = errors.New("error initializing keydir")
+
+	ErrEncodingHeaderFailed = errors.New("encoding fail: failed to encode header")
+	ErrDecodingHeaderFailed = errors.New("decoding fail: failed to decode header")
+
+	ErrEncodingKVFailed = errors.New("encoding fail: failed to encode kv")
+	ErrDecodingKVFailed = errors.New("decoding fail: failed to decode kv")
+)
