a63a02cd6edf1dbebd271aadfe73b31f03f19e1c
tferdous17 <tferdous611@gmail.com>
2024-09-24 17:09:16 -0400

    implement CheckSum for data integrity and CalculateChecksum()

--- internal/kvformat.go
+++ internal/kvformat.go
@@ -4,6 +4,7 @@ import (
 	"bitcask-go/utils"
 	"bytes"
 	"encoding/binary"
+	"hash/crc32"
 )
 
 /*
@@ -14,7 +15,7 @@ The format for each key-value on disk is as follows:
 timestamp, key_size, value_size form the header of the entry and each of these must be 4 bytes at most
 thus header size is fixed at a length of 12 bytes
 */
-const headerSize = 12
+const headerSize = 16
 
 // KeyEntry holds metadata about the KV pair, which is what we will insert into the keydir
 type KeyEntry struct {
@@ -24,6 +25,7 @@ type KeyEntry struct {
 }
 
 type Header struct {
+	CheckSum  uint32
 	TimeStamp uint32
 	KeySize   uint32
 	ValueSize uint32
@@ -56,7 +58,8 @@ func NewHeader(buf []byte) (*Header, error) {
 }
 
 func (h *Header) EncodeHeader(buf *bytes.Buffer) error {
-	err := binary.Write(buf, binary.LittleEndian, &h.TimeStamp)
+	err := binary.Write(buf, binary.LittleEndian, &h.CheckSum)
+	binary.Write(buf, binary.LittleEndian, &h.TimeStamp)
 	binary.Write(buf, binary.LittleEndian, &h.KeySize)
 	binary.Write(buf, binary.LittleEndian, &h.ValueSize)
 
@@ -70,9 +73,10 @@ func (h *Header) EncodeHeader(buf *bytes.Buffer) error {
 func (h *Header) DecodeHeader(buf []byte) error {
 
 	// must pass in reference b/c go is call by value and won't modify original otherwise
-	_, err := binary.Decode(buf[:4], binary.LittleEndian, &h.TimeStamp)
-	binary.Decode(buf[4:8], binary.LittleEndian, &h.KeySize)
-	binary.Decode(buf[8:12], binary.LittleEndian, &h.ValueSize)
+	_, err := binary.Decode(buf[:4], binary.LittleEndian, &h.CheckSum)
+	binary.Decode(buf[4:8], binary.LittleEndian, &h.TimeStamp)
+	binary.Decode(buf[8:12], binary.LittleEndian, &h.KeySize)
+	binary.Decode(buf[12:16], binary.LittleEndian, &h.ValueSize)
 
 	if err != nil {
 		return utils.ErrDecodingHeaderFailed
@@ -100,4 +104,26 @@ func (r *Record) DecodeKV(buf []byte) error {
 
 func (r *Record) Size() uint32 {
 	return r.RecordSize
+}
+
+func (r *Record) CalculateChecksum() uint32 {
+	/*
+		compute a checksum for the ENTIRE entry, not just the value itself
+		i suppose this is more secure? since it accounts for if ANY fields change
+
+		crc.checksum takes a []byte buf as input, so lets try encoding the entire record into a
+		byte buffer and then calculate the checksum based on that
+		we only want to calculate it from tstamp...value so go from [4:]
+	*/
+	headerBuf := new(bytes.Buffer)
+	binary.Write(headerBuf, binary.LittleEndian, &r.Header.TimeStamp)
+	binary.Write(headerBuf, binary.LittleEndian, &r.Header.KeySize)
+	binary.Write(headerBuf, binary.LittleEndian, &r.Header.ValueSize)
+
+	// cant append a []byte directly into a []byte, so destructure the val into indiv bytes
+	kvBuf := append([]byte(r.Key), []byte(r.Value)...)
+
+	buf := append(headerBuf.Bytes(), kvBuf...)
+
+	return crc32.ChecksumIEEE(buf)
 }
