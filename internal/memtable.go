206b38d27859824f0667d20838e2bee1249d5687
tferdous17 <tferdous611@gmail.com>
2024-10-05 17:34:18 -0400

    implement rudimentary Flush mechanism on memtable

--- internal/memtable.go
+++ internal/memtable.go
@@ -2,14 +2,16 @@ package internal
 
 type Memtable struct {
 	data RedBlackTree
+	size int
 }
 
 func NewMemtable() *Memtable {
-	return &Memtable{RedBlackTree{root: nil}}
+	return &Memtable{RedBlackTree{root: nil}, 0}
 }
 
 func (m *Memtable) Put(key string, value Record) {
 	m.data.Insert(key, value)
+	m.size++
 }
 
 func (m *Memtable) Get(key string) (Record, error) {
@@ -17,5 +19,17 @@ func (m *Memtable) Get(key string) (Record, error) {
 }
 
 func (m *Memtable) PrintAllRecords() {
-	m.data.Inorder()
+}
+
+func (m *Memtable) Flush(filename string) {
+	sortedEntries := m.data.ReturnAllRecordsInSortedOrder()
+	table := NewSSTable(filename)
+	table.writeEntriesToSST(sortedEntries)
+	m.clear()
+}
+
+func (m *Memtable) clear() {
+	// clear table once flushed to SSTable
+	m.data.root = nil
+	m.size = 0
 }
