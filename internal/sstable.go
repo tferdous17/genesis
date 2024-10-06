cc8881da136b7a5e903ddfdea3f5e59576b6f8a2
tferdous17 <tferdous611@gmail.com>
2024-10-06 13:36:51 -0400

    include sstCounter var for new sstable file names

--- internal/sstable.go
+++ internal/sstable.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"fmt"
 	"os"
+	"sync/atomic"
 )
 
 const (
@@ -12,27 +13,36 @@ const (
 	INDEX_FILE_EXTENSION string = ".index"
 )
 
+var sstTableCounter uint32
+
 type SSTable struct {
-	dataFile  *os.File
-	indexFile *os.File
+	dataFile   *os.File
+	indexFile  *os.File
+	sstCounter uint32
 }
 
 func NewSSTable(filename string) *SSTable {
 	dataFile, indexFile := initializeFromDisk(filename)
+	atomic.AddUint32(&sstTableCounter, 1)
 	return &SSTable{
-		dataFile:  dataFile,
-		indexFile: indexFile,
+		dataFile:   dataFile,
+		indexFile:  indexFile,
+		sstCounter: sstTableCounter,
 	}
 }
 
 func initializeFromDisk(filename string) (*os.File, *os.File) {
 	// create data and index files
-	dataFile, err := os.Create(filename + DATA_FILE_EXTENSION)
-	indexFile, err := os.Create(filename + INDEX_FILE_EXTENSION)
+	dataFile, err := os.OpenFile(filename+DATA_FILE_EXTENSION, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0666)
+	indexFile, err := os.OpenFile(filename+INDEX_FILE_EXTENSION, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0666)
 	if err != nil {
 		fmt.Println("init file err", err)
 	}
 	return dataFile, indexFile
+}
+
+func (sst *SSTable) getNextSstFilename(directory string) string {
+	return fmt.Sprintf("%s/sst_%d", directory, sst.sstCounter)
 }
 
 func (sst *SSTable) writeEntriesToSST(entries []Record) {
