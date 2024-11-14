baef02771893ce1f8181c1e9bc0e7f44467d9453
tferdous17 <tferdous611@gmail.com>
2024-11-14 11:10:27 -0500

    implement HTTP layer for client requests

--- http/service.go
+++ http/service.go
@@ -1,28 +1,25 @@
 package http
 
 import (
+	"bitcask-go/store"
 	"bitcask-go/utils"
+	"encoding/json"
+	"fmt"
+	"io"
 	"net"
 	"net/http"
+	"strings"
 )
-
-//type Store interface {
-//	Put(key string, value string) error
-//	Get(key string) (string, error)
-//	Close() bool
-//}
-
-// reference this repo: https://github.com/otoolep/go-httpd/blob/master/httpd/service.go
 
 type Service struct {
 	addr string
 	ln   net.Listener
 
-	store Store
+	store store.Store
 }
 
 // NewService returns an unitialized HTTP service
-func NewService(addr string, store Store) *Service {
+func NewService(addr string, store store.Store) *Service {
 	return &Service{
 		addr:  addr,
 		store: store,
@@ -58,11 +55,67 @@ func (s *Service) Close() error {
 }
 
 func (s *Service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	panic("implement this")
+	if strings.HasPrefix(r.URL.Path, "/key") {
+		s.handleKeyRequest(w, r)
+	} else {
+		fmt.Println("does not have /key prefix")
+		w.WriteHeader(http.StatusNotFound)
+	}
 }
 
 func (s *Service) handleKeyRequest(w http.ResponseWriter, r *http.Request) {
+	getKey := func() string {
+		parts := strings.Split(r.URL.Path, "/")
+		if len(parts) != 3 {
+			return ""
+		}
+		return parts[2]
+	}
 
+	switch r.Method {
+	case "POST":
+		b, err := io.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+		m := map[string]string{}
+		if err := json.Unmarshal(b, &m); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+
+		for k, v := range m {
+			if err := s.store.Put(&k, &v); err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+		}
+
+	case "GET":
+		k := getKey()
+		if k == "" {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+		val, err := s.store.Get(k)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		io.WriteString(w, val)
+
+	case "DELETE":
+		k := getKey()
+		if k == "" {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+		err := s.store.Delete(k)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+	return
 }
 
 func (s *Service) Addr() net.Addr {
