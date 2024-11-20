87421dac3e9f52357b2898b266eb135689b242f1
tferdous17 <tferdous611@gmail.com>
2024-11-19 19:52:35 -0500

    many changes, including:
    Started up a gRPC server on every node upon start, implemented a basic TransferDataBetweenNodes func, created helper funcs for Record <--> proto.KVPair conversion, atomically increment node port

--- store/cluster.go
+++ store/cluster.go
@@ -2,17 +2,22 @@ package store
 
 import (
 	"bitcask-go/http"
+	"bitcask-go/proto"
+	"bitcask-go/utils"
+	"context"
 	"fmt"
 	"github.com/serialx/hashring"
 	"log"
 	"os"
 	"os/signal"
+	"sync/atomic"
 	"syscall"
+	"time"
 )
 
 type Node struct {
 	ID    string
-	addr  string
+	Addr  string
 	Store *DiskStore
 }
 
@@ -21,7 +26,8 @@ type Cluster struct {
 	nodes    map[string]*Node
 }
 
-var startingNodePort = 11000
+var nodeCounter uint32 = 1
+var currentNodePort uint32 = 11000
 
 func (c *Cluster) initNodes(numOfNodes int) {
 	c.nodes = make(map[string]*Node)
@@ -30,16 +36,92 @@ func (c *Cluster) initNodes(numOfNodes int) {
 	for i := 0; i < numOfNodes; i++ {
 		store, _ := NewDiskStore()
 		node := Node{
-			ID:    fmt.Sprintf("node-%d", i+1),
-			addr:  fmt.Sprintf(":%d", startingNodePort),
+			ID:    fmt.Sprintf("node-%d", nodeCounter),
+			Addr:  fmt.Sprintf(":%d", currentNodePort),
 			Store: store,
 		}
-		c.nodes[node.addr] = &node
-		startingNodePort++
-		nodeAddrs = append(nodeAddrs, node.addr)
+		c.nodes[node.Addr] = &node
+
+		StartGRPCServer(node.Addr, &node)
+
+		atomic.AddUint32(&currentNodePort, 1)
+		atomic.AddUint32(&nodeCounter, 1)
+		nodeAddrs = append(nodeAddrs, node.Addr)
 	}
 
 	c.hashRing = hashring.New(nodeAddrs)
+}
+
+func (c *Cluster) TransferDataBetweenNodes(srcNodeAddr string, destNodeServerAddr string) {
+	client, conn := StartGRPCClient(destNodeServerAddr)
+	defer conn.Close()
+
+	rec := []Record{
+		{
+			Header: Header{
+				CheckSum:  1,
+				Tombstone: 2,
+				TimeStamp: 3,
+				KeySize:   4,
+				ValueSize: 5,
+			},
+			Key:        "tesdfdsfds",
+			Value:      "dbvdbdf",
+			RecordSize: 10,
+		},
+		{
+			Header: Header{
+				CheckSum:  4,
+				Tombstone: 1,
+				TimeStamp: 7,
+				KeySize:   6,
+				ValueSize: 5,
+			},
+			Key:        "dfgdfgdfg",
+			Value:      "xvv",
+			RecordSize: 14,
+		},
+	}
+
+	kvPairs := convertRecordsToProtoKVPairs(&rec)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	res, err := client.MigrateKeyValuePairs(ctx, &proto.KeyValueMigrationRequest{
+		SourceNodeAddr: srcNodeAddr,
+		DestNodeAddr:   destNodeServerAddr,
+		KvPairs:        kvPairs,
+	})
+	if err != nil {
+		utils.LogRED("err = %s", err)
+	}
+
+	fmt.Println(res)
+	//transferKVPairs(client, srcNodeAddr, destNodeServerAddr, &rec)
+}
+
+func (c *Cluster) AddNode() {
+	store, _ := NewDiskStore()
+	node := Node{
+		ID:    fmt.Sprintf("node-%d", nodeCounter),
+		Addr:  fmt.Sprintf(":%d", currentNodePort),
+		Store: store,
+	}
+	c.nodes[node.Addr] = &node
+	atomic.AddUint32(&nodeCounter, 1)
+	atomic.AddUint32(&currentNodePort, 1)
+
+	// ? reassign hashring to whats returned by AddNode()?
+	c.hashRing = c.hashRing.AddNode(node.Addr)
+}
+
+func (c *Cluster) RemoveNode(addr string) {
+	_, ok := c.nodes[addr]
+	if ok {
+		c.hashRing.RemoveNode(addr)
+	} else {
+		fmt.Printf("node @ addr %s not found", addr)
+	}
 }
 
 var defaultPort = ":8080"
@@ -52,15 +134,26 @@ func (c *Cluster) Open() {
 	signalCh := make(chan os.Signal, 1)
 	signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM)
 
+	fmt.Println("calling transfer nodes")
+	// * works
+	c.TransferDataBetweenNodes(":11000", ":11001")
+
+	res, err := c.nodes[":11001"].Store.Get("dfgdfgdfg")
+	if err != nil {
+		utils.LogRED("err = %s", err)
+	}
+	utils.LogYELLOW("res after transferring = %s", res)
+
 	// Block until one of the signals above is received
 	select {
 	case <-signalCh:
+		c.PrintDiagnostics()
 		log.Println("signal received, shutting down...")
 		err := clusterService.Close()
 		if err != nil {
 			fmt.Println(err)
 		}
-		c.PrintDiagnostics()
+
 	}
 }
 
@@ -105,7 +198,46 @@ func (c *Cluster) Delete(key string) error {
 func (c *Cluster) PrintDiagnostics() {
 	fmt.Println("DIAGNOSTICS:")
 	for _, v := range c.nodes {
-		fmt.Printf(v.ID + " @ address " + v.addr + " , num keys: ")
+		fmt.Printf(v.ID + " @ address " + v.Addr + " , num keys: ")
 		v.Store.LengthOfMemtable()
 	}
+}
+
+func convertProtoRecordToStoreRecord(record *proto.Record) *Record {
+	return &Record{
+		Header: Header{
+			CheckSum:  record.Header.Checksum,
+			Tombstone: uint8(record.Header.Tombstone),
+			TimeStamp: record.Header.Timestamp,
+			KeySize:   record.Header.KeySize,
+			ValueSize: record.Header.ValueSize,
+		},
+		Key:        record.Key,
+		Value:      record.Value,
+		RecordSize: record.RecordSize,
+	}
+}
+
+func convertRecordsToProtoKVPairs(records *[]Record) []*proto.KVPair {
+	var KVPairs []*proto.KVPair
+	for _, rec := range *records {
+		convRec := &proto.KVPair{
+			Record: &proto.Record{
+				Header: &proto.Header{
+					Checksum:  rec.Header.CheckSum,
+					Tombstone: uint32(rec.Header.Tombstone),
+					Timestamp: rec.Header.TimeStamp,
+					KeySize:   rec.Header.KeySize,
+					ValueSize: rec.Header.ValueSize,
+				},
+				Key:        rec.Key,
+				Value:      rec.Value,
+				RecordSize: rec.RecordSize,
+			},
+		}
+
+		KVPairs = append(KVPairs, convRec)
+	}
+
+	return KVPairs
 }
